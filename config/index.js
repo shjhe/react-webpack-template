c46aef79e4298cdd8317d24bbb89cae63bc7df81
shjhe <568675732@qq.com>
2018-07-27 10:01:24 +0800

    更新配置

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ module.exports = {
         target: 'http://192.168.31.40:8080',
         changeOrigin: true,
         pathRewrite: {
-          '^api': 'api'
+          '^api': '/api'
         }
       }
     },
