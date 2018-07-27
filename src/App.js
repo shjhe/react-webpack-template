29a74e70f55c81d3ff54476390ad89b6a8e466b3
shjhe <568675732@qq.com>
2018-07-27 15:44:52 +0800

    临时存储

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Tag } from 'antd-mobile';
 class App extends Component {
   render() {
     return (
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Tag selected>selected</Tag>
       </div>
     );
   }
